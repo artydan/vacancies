b82d7e6aea6107e4e71c580418fc4fc6fc7bb2ad
Artur Danielyan <burrito0101@gmail.com>
2021-04-12 15:46:53 +0300

    delete index.css

--- src/components/VacCard.js
+++ src/components/VacCard.js
@@ -1,43 +1,48 @@
 import React from "react";
 import "../global.css";
 
-function Vaccard({vacancie, openVacancieHandler}) {
- 
+function Vaccard({ vacancie, openVacancieHandler }) {
   return (
     <div>
       <div className="vacCard">
         <div className="vacName">{vacancie.name}</div>
         {vacancie.snippet?.responsibility && (
-              <div className="vacResp">
-                {vacancie.snippet?.responsibility}
-              </div>
-            )}
+          <div className="vacResp">{vacancie.snippet?.responsibility}</div>
+        )}
         {vacancie.employer.logo_urls?.original && (
           <div className="logo">
-            <img className="imLogo" src={vacancie.employer.logo_urls?.original} alt="logo" />
+            <img
+              className="imLogo"
+              src={vacancie.employer.logo_urls?.original}
+              alt="logo"
+            />
           </div>
         )}
         {(vacancie.salary?.from && vacancie.salary?.to && (
           <div className="vacSalary">
-            {vacancie.salary?.from.toLocaleString("ru-RU")} - {vacancie.salary?.to.toLocaleString("ru-RU")}{" "}
-            {vacancie.salary?.currency === "RUR" ? "RUB": vacancie.salary?.currency}
+            {vacancie.salary?.from.toLocaleString("ru-RU")} -{" "}
+            {vacancie.salary?.to.toLocaleString("ru-RU")}{" "}
+            {vacancie.salary?.currency === "RUR"
+              ? "RUB"
+              : vacancie.salary?.currency}
           </div>
-        )) 
-          ||
+        )) ||
           (vacancie.salary?.from && (
             <div className="vacSalary">
               {vacancie.salary?.from.toLocaleString("ru-RU")}{" "}
-              {vacancie.salary?.currency === "RUR" ? "RUB": vacancie.salary?.currency}
+              {vacancie.salary?.currency === "RUR"
+                ? "RUB"
+                : vacancie.salary?.currency}
             </div>
           )) ||
           (vacancie.salary?.to && (
             <div className="vacSalary">
               {vacancie.salary?.to.toLocaleString("ru-RU")}{" "}
-              {vacancie.salary?.currency === "RUR" ? "RUB": vacancie.salary?.currency}
+              {vacancie.salary?.currency === "RUR"
+                ? "RUB"
+                : vacancie.salary?.currency}
             </div>
-          )) ||
-          <div className="vacSalary">Зп не указана</div>
-          }
+          ))}
 
         <div className="vacCity"> {vacancie.address?.city}</div>
         <div className="btn_wrapper">
